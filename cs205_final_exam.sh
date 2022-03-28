d4f0559dcb5edc343aaa930001e9dc8f57aa32ac
tn334 <tn334@nau.edu>
2022-03-27 21:45:42 -0700

    not final attempt but written functioning code

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -15,3 +15,36 @@
 # The data file will be passed in to the script as a positional parameter and will not
 # necessarily be called pokemon.dat. However, you can assume that any file passed to this
 # script will be formatted exactly the way pokemon.dat is formatted.
+
+BEGIN{FS="\t"}
+{
+#check for nonLeg only 
+if(NR12!= "FALSE"){
+#add total of nonLegendary pokemon
+nonLeg +=$1
+
+#add avg of HP
+hp +=$6
+#calc avg
+avgHp = (hp/nonLeg)
+
+#defense sum
+defenseSum +=$8
+
+#avg defense
+avgDef =( defenseSum/nonLeg)
+
+header = "======= SUMMARY OF POKEMON.DAT ======"
+closer = "======== END SUMMARY ======"
+}}
+END{
+#print header
+printf (header)
+printf"\n"
+printf ("   Total Non-Legendary Pokemon: [%i]", nonLeg)
+printf"\n"
+printf("    Avg. HP: [%i]", avgHp)
+printf("\n")
+printf("    Avg. Defense: [%i]", avgDef)
+printf("\n")
+printf(closer)}
